c709db901010ba66b0118483d0820e6134e8fadf
xinyang.li <xinyang.li@thoughtworks.com>
2024-10-12 12:39:34 +0800

    [HF-125][Xinyang]fix: add add custom serializer module instead of overwrite

--- src/main/java/edu/stanford/fsi/reap/config/JacksonConfig.java
+++ src/main/java/edu/stanford/fsi/reap/config/JacksonConfig.java
@@ -7,24 +7,21 @@ import edu.stanford.fsi.reap.converter.LocalDateTimeDeserializer;
 import edu.stanford.fsi.reap.converter.LocalDateTimeSerializer;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JacksonConfig {
 
-  @Bean
-  public Jackson2ObjectMapperBuilderCustomizer localDateTimeCustomizer() {
-    return builder -> {
-      SimpleModule module = new SimpleModule();
+  @Bean("localDateTimeCustomizerModule")
+  public SimpleModule localDateTimeCustomizerModule() {
+    SimpleModule module = new SimpleModule();
 
-      module.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer());
-      module.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer());
-      module.addSerializer(LocalDate.class, LocalDateSerializer.INSTANCE);
-      module.addDeserializer(LocalDate.class, LocalDateDeserializer.INSTANCE);
+    module.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer());
+    module.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer());
+    module.addSerializer(LocalDate.class, LocalDateSerializer.INSTANCE);
+    module.addDeserializer(LocalDate.class, LocalDateDeserializer.INSTANCE);
 
-      builder.modules(module);
-    };
+    return module;
   }
 }
